8f62585ef4dcc29bc6ea32c4c8648d894d231d72
Pedro Henrique <phjfarias@hotmail.com>
2020-08-30 23:13:03 -0300

    fix: add port variable to app

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: python app.py
+web: gunicorn --bind 0.0.0.0:${PORT} wsgi
+
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from api import app
 from api import controllers
+import os
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
